b90a6e4ccacfc78c081ccd8a98c562c0e276a9e6
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-05-19 10:19:21 +0200

    Updates

--- assets/NotFound-cd26244d.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-47d36eb3.js";const o=()=>s.jsx("div",{className:"notFound",children:s.jsx("div",{className:"container",children:"Такой страницы не существует"})});export{o as default};
---
+++ assets/NotFound-f2a998c7.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-575a5161.js";const o=()=>s.jsx("div",{className:"notFound",children:s.jsx("div",{className:"container",children:"Такой страницы не существует"})});export{o as default};
