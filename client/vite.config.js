8dd84b20784ba228a08174addcd68f3d5967d29f
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-05-20 23:15:44 +0200

    zx

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://185.185.70.171:3004",
-        secure: false,
-      },
-    },
-  },
-
   plugins: [react()],
-});
+})
