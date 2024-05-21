59edd6355f2fe9a7fedcbbaecc05b51796b7b417
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-05-21 10:12:05 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-=======
-  base: '/',
-  server: {
-    // proxy: {
-    //   "/api": {
-    //     target: "http://185.185.70.171:3004",
-    //     secure: false,
-    //   },
-    // },
-  },
-
->>>>>>> eeee81cc1f565c6a79eea2744c2de91d95d9cb03
   plugins: [react()],
 })
